04d2e8bd4ca78cd350857bf81d7c4bcb53489bee
Arc-lin <m13643046965@163.com>
2016-02-18 22:13:40 +0800

    电子名片详情显示

---
+++ WeChat/Classes/Profile/Controller/ALProfileViewController.h
@@ -0,0 +1,13 @@
+//
+//  ALProfileViewController.h
+//  WeChat
+//
+//  Created by Arclin on 16/2/18.
+//  Copyright © 2016年 sziit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ALProfileViewController : UITableViewController
+
+@end
